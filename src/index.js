a5b8e7574c836e94c1e557821a6a0b7498edc38b
Andrej <andrej.bosanac@student.csulb.edu>
2022-11-18 16:07:51 -0800

    Books structure added

--- src/index.js
+++ src/index.js
@@ -1,5 +1,31 @@
+import ReactDom from "react-dom";
 
+function BookList() {
+  return (
+    <section>
+      <Book />
+    </section>
+  );
+}
 
+const Book = () => {
+  return (
+    <article>
+      <Image />
+      <Title />
+      <Author />
+    </article>
+  );
+};
 
+const Image = () => (
+  <img
+    src="https://images-na.ssl-images-amazon.com/images/I/61xWTfK4EKL._AC_UL210_SR195,210_.jpg"
+    alt=""
+  />
+);
 
-import 
+const Title = () => <h1>Engineering in plain sight</h1>;
+const Author = () => <h1>Michael Morpugo</h1>;
+
+ReactDom.render(<BookList />, document.getElementById("root"));
