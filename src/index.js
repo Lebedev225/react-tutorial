a2af3843616711401e43ba238ebebf3a8ede3819
Andrej <andrej.bosanac@student.csulb.edu>
2022-11-25 02:25:58 -0800

    Rewritten using props

--- src/index.js
+++ src/index.js
@@ -1,41 +1,46 @@
 import ReactDom from "react-dom";
-
 // CSS
 import "./index.css";
-function BookList() {
-  return (
-    <section className="booklist">
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-    </section>
-  );
-}
 
-const Book = () => {
-  return (
-    <article className="book">
-      <Image />
-      <Title />
-      <Author />
-    </article>
-  );
+//Initialize variables
+
+const firstBook = {
+    title: "Engineering in plain sight",
+    author: "Michael Morpugo",
+    img: "https://images-na.ssl-images-amazon.com/images/I/61xWTfK4EKL._AC_UL210_SR195,210_.jpg",
 };
 
-const Image = () => (
-  <img
-    src="https://images-na.ssl-images-amazon.com/images/I/61xWTfK4EKL._AC_UL210_SR195,210_.jpg"
-    alt=""
-  />
-);
+const secondBook = {
+    title: "Atomic Habits",
+    author: "James Clear",
+    img: "https://images-na.ssl-images-amazon.com/images/I/81wgcld4wxL._AC_UL210_SR195,210_.jpg",
+};
 
-const Title = () => <h1>Engineering in plain sight</h1>;
-const Author = () => <h1>Michael Morpugo</h1>;
+function BookList() {
+    return (
+        <section className="booklist">
+            <Book
+                img={firstBook.img}
+                title={firstBook.title}
+                author={firstBook.author}
+            />
+            <Book
+                img={secondBook.img}
+                title={secondBook.title}
+                author={secondBook.author}
+            />
+        </section>
+    );
+}
+
+const Book = (props) => {
+    return (
+        <article className="book">
+            <img src={props.img} alt="" />
+            <h1>{props.title}</h1>
+            <h1>{props.author}</h1>
+        </article>
+    );
+};
 
 ReactDom.render(<BookList />, document.getElementById("root"));
